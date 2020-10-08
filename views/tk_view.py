96f2706c27c35987bc09a3e95888b81d266e2647
Duarte Elvas <duartecelvas@gmail.com>
2020-10-08 12:38:26 +0100

    Working and going strong

--- views/tk_view.py
+++ views/tk_view.py
@@ -4,11 +4,15 @@ from controllers.controller import controller
 class tk_view:
     def __init__(self):
         self.controller=controller()
+        self.controller.open("https://web.whatsapp.com/")
         #?------------------Creating-the-main-screen-----------------------------
         self.app = tkinter.Tk() #Making the main root
         self.app.geometry("200x200") # Setting the size of the main window
         self.app.title("Cd Burner") 
         #self.app.iconbitmap("icons/icon.ico") #Setting the main left top icon
         #?-----------------Populating-the-main-screen----------------------------
-        self.button_main = tkinter.Button(master=self.app,text="Start",command=lambda:print("oi"),height = 5, width = 9)
+        self.button_main = tkinter.Button(master=self.app,text="Start",command=self.run_time,height = 5, width = 9)
         self.button_main.pack(padx=10,pady=40)
+
+    def run_time(self):
+        self.controller.do_it("adoro-te",10)
